9aa12b2d32b7652a66d4cf5aa88f01a4ec35707b
Кремлёв Святослав Юрьевич <s.kremlev@voskhod.ru>
2021-10-16 11:18:27 +0300

    Init

---
+++ src/main/java/module-info.java
@@ -0,0 +1,1 @@
+module svkreml.GraalVmMavenHelloWorld {}
---
+++ src/main/java/svkreml/GraalVmMavenHelloWorld/HelloWorld.java
@@ -0,0 +1,7 @@
+package svkreml.GraalVmMavenHelloWorld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
